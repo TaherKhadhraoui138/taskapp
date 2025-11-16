680a43d6dd10a7ea43f8da540aa54a3c120b6d69
AbdouBenHatttab <abderrahmen.benhattab@bizerte.r-iset.tn>
2025-11-16 18:55:10 +0100

    firebase with auth

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 
 // Définition des couleurs du thème
@@ -103,7 +105,10 @@ class TaskManagerApp extends StatelessWidget {
   }
 }
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TaskManagerApp());
 }
