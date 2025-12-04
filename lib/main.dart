7ecbce56b1b4360c4c801a39f114d564d068e3b9
Youssef <limemyoussef@bizerte.r-iset.tn>
2025-12-04 22:17:13 +0100

    aading the IA model in the sub-task suggestion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
@@ -74,7 +75,7 @@ void main() async {
 
   // Initialize notification service
   await NotificationService().initialize();
-
+  Gemini.init(apiKey: 'AIzaSyAzbAROUqnZUFELrPPQzIfcmAwoTISqAlY');
   runApp(
     MultiProvider(
       providers: [
